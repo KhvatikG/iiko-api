5e56a76ec07dbf297703750ce578470683c0376e
Garik <khvatik.igor@gmail.com>
2025-03-19 12:00:45 +0300

    add types param to get_dishes

--- src/iiko_api/endpoints/dishes.py
+++ src/iiko_api/endpoints/dishes.py
@@ -9,22 +9,29 @@ class DishesEndpoints:
     def __init__(self, client: BaseClient):
         self.client = client
 
-    def get_dishes(self, articles: list[str] | None = None, ids: list[str] | None = None):
+    def get_dishes(self,
+                   articles: list[str] | None = None,
+                   ids: list[str] | None = None,
+                   types: list[str] | None = None):
         """
-        Получение списка блюд, по артикулу или по id
+        Получение списка элементов номенклатуры, по артикулу, по id и по типу элемента номенклатуры.
+
+        :param types: список типов элементов номенклатуры, по которым необходимо получить список блюд, если None - получить все блюда
         :param articles: список артикулов, по которым необходимо получить список блюд, если None - получить все блюда
         :param ids: список id блюд, по которым необходимо получить список блюд, если None - получить все блюда
         :return: список словарей, где каждый словарь представляет блюдо
         """
         url = "/resto/api/v2/entities/products/list"
+        url += "?"
         if articles:
-            url += "?"
             for article in articles:
                 url += f"nums={article}&"
         if ids:
-            url += "?"
             for id_ in ids:
                 url += f"ids={id_}&"
+        if types:
+            for type_ in types:
+                url += f"types={type_}&"
 
         # Авторизация
         self.client.login()
